6969c74a0581e8e13c936cbfcaef6806d49d446c
ivicak99 <85429752+ivicak99@users.noreply.github.com>
2022-12-10 20:16:22 +0100

    Update aufgabe_2_abcd.hs

--- Übung 7/aufgabe_2_abcd.hs
+++ Übung 7/aufgabe_2_abcd.hs
@@ -6,6 +6,21 @@
 
 module Aufgabe2 where
 
+-- a)
+-- Vor.: Eingabe einer Liste mit ganzen Zahlne
+-- Erg.: Ausgabe eines Tupels; der erste Wert sagt aus wie viele Zahlen <0 und der zweite wie viele Zahlen >0 in der Liste sind.
     lowhigh :: [Int] -> (Int,Int)
     lowhigh [] = (0,0)
     lowhigh xs = (length(filter (>0) xs), length(filter (>0) xs))
+
+-- c)
+-- Vor.: Eingabe einer Liste mit Zahlen, Gleitkommerzahlen
+-- Erg.: Der Betrag derjenigen Zahl, die den keleinsten Abstand zur 0 hat.
+    absmin :: [Double] -> Double
+    absmin xs = minimum(map abs xs)
+
+-- d)
+-- Vor.: Die Eingegebene Funktion muss einen Bool'schen Wert als Ausgabe haben und die Liste muss aus ganzen Zahlne bestehen.
+-- Erg.: Eine Liste mit Bool'schen Werten 
+    toBools :: (Int -> Bool) -> [Int] -> [Bool]
+    toBools p xs = map p xs
